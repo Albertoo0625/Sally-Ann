398ea7e248fa8eed185b3dd5d675f55da934962f
Albert_Too <tooalbert79@gmail.com>
2024-07-15 23:24:12 +0300

    first commit

--- src/Components/Home/HomeFooter.jsx
+++ src/Components/Home/HomeFooter.jsx
@@ -16,7 +16,7 @@ const HomeFooter = () => {
     
       <div  className='VideoTour'>
        <div className='gradient'></div>
-       <video className='videoasset' width="auto" height="auto" controls>
+       <video style={{maxHeight:"360px",maxWidth:'640px'}}className='videoasset' width="auto" height="auto" controls>
         <source src='assets/video/video.mp4' type="video/mp4"/>
         Your browser does not support the video tag.
       </video>
