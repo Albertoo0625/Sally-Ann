b8b492243d7d900a3a5db4713607d411911796b1
Albert_Too <tooalbert79@gmail.com>
2024-07-18 17:11:52 +0300

    Images css added

--- src/Components/Home/HomeCategories.jsx
+++ src/Components/Home/HomeCategories.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomeCategories = () => {
   return (
-    <div className='Container' style={{}}>
+    <div className='Container' style={{backgroundImage:`url(https://brookhurstschool.or.ke/wp-content/uploads/2023/09/black-ptn.png)`}}>
         <div className='preprep'>
         <img src='assets/images/childrenplaying.webp' className='preprepImage'></img>
         </div>
